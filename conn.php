62ab312609dc2b04c5454281d752b78f8d98a12c
Chigozirim Justice Uzor <kescopee@gmail.com>
2017-03-03 01:47:56 +0000

    First commit of contact class and connection files

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+
+function db_connect(){
+	$db_host = 'localhost';
+	$db_user = 'root';
+	$db_pass = '';
+	$db_name = 'contacts';
+
+	$out = new mysqli($db_host, $db_user, $db_pass, $db_name) or die($out->connect_error);
+	return $out;
+}
+
+?>
---
+++ contact.class.php
@@ -0,0 +1,78 @@
+<?php
+
+include "conn.php";
+
+class Contact{
+
+	private $_db;
+	private $_name;
+	private $_email;
+	private $_tel_no;
+
+	function __construct(){}
+
+	function create($name, $email, $tel_no){
+		$this->_db = db_connect();
+		$this->_name = $this->_db->real_escape_string($name);
+		$this->_email = $this->_db->real_escape_string($email);
+		$this->_tel_no = $this->_db->real_escape_string($tel_no);
+		$query = "
+			INSERT INTO contact_details (name, email, tel_no)
+			VALUES ('" . $this->_name  . "','" . $this->_email . "','" . $this->_tel_no . "')
+		";
+		$saved = $this->_db->query($query);
+		$output = '';
+		if(!$saved){
+			$output =  'Save unsuccessful: '.$this->_db->error;
+		}else{
+			$output = 'Save successful';
+		}
+		$this->_db->close();
+		return $output;
+	}
+
+	function read(){
+		$this->_db = db_connect();
+		$query = "SELECT * FROM contact_details ORDER BY name ASC";
+		$result = $this->_db->query($query);
+		$this->_db->close();
+		return $result;
+	}
+
+	function update($id, $contact_array){
+		$this->_db = db_connect();
+		$this->_name = $this->_db->real_escape_string($contact_array['name']);
+		$this->_email = $this->_db->real_escape_string($contact_array['email']);
+		$this->_tel_no = $this->_db->real_escape_string($contact_array['tel_no']);
+		$query = "
+			UPDATE contact_details 
+			SET name='".$this->_name."', email='".$this->_email."', tel_no='".$this->_tel_no."'
+			WHERE id = '".$id."'
+		";
+		$updated = $this->_db->query($query);
+		$output = '';
+		if(!$updated){
+			$output = 'Update unsuccessful: '.$this->_db->error;
+		}else{
+			$output = 'Update successful';
+		}
+		$this->_db->close();
+		return $output;
+	}
+
+	function delete($id){
+		$this->_db = db_connect();
+		$query = "DELECT FROM contact_details WHERE id=".$id;
+		$deleted = $this->_db->query($query);
+		$output = '';
+		if(!$deleted){
+			$output = 'Delete unsuccessful: '.$this->_db->error;
+		}else{
+			$output = 'Delete successful';
+		}
+		$this->_db->close();
+		return $output;
+	}
+}
+
+?>
